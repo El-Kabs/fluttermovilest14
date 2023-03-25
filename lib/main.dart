c7bd5cf117fec5f5a076649c0012c325927b3c1d
kevin.santos <kevin.santos@COC02WK42ZHTD5.local>
2023-03-25 14:37:16 -0500

    Map view

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
-import 'views/splash.dart';
-import 'views/login.dart';
 import 'views/map.dart';
-import 'views/register.dart';
-import 'services/auth.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-
-  final AuthService _auth = AuthService();
-  final bool isLogged = await _auth.isLogged();
   runApp(
     MaterialApp(
-      initialRoute: isLogged ? '/map' : '/',
+      initialRoute: '/map',
       routes: {
-        '/': (context) => const SplashScreen(),
-        '/login': (context) => const LoginScreen(),
         '/map': (context) => MapScreen(),
-        '/register':(context) => RegisterScreen()
       },
     ),
   );
